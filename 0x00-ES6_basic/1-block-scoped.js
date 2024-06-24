35ebc85161747160ea64649fe58416e9a70acf66
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-24 15:46:35 +0200

    Modified the variables of the function taskBlock so they are not overwritten inside a conditional block.

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,7 +4,7 @@ export default function taskBlock(trueOrFalse) {
 
   if (trueOrFalse) {
     const task = true; // eslint-disable-line no-unused-vars
-    const task2 = false;
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
