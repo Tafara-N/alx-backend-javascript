344fa209ca860d9a59abed664868cecb61f67f5d
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-06-26 21:11:38 +0200

    Await / Async.

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+	let photo;
+	let user;
+	try {
+		photo = await uploadPhoto();
+		user = await createUser();
+	} catch (e) {
+		photo = null;
+		user = null;
+	}
+	return { photo, user };
+}
