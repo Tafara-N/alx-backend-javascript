21aac1b7e4249b1d88062d13775f3b91d52a1c9c
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-20 21:29:13 +0200

    Linking: `/` -> AppController and `/students`, `/students/:majorto` -> StudentsController.

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -2,22 +2,16 @@
  * This file contains the routing for the application
  */
 
-const express = require("express");
-const AppController = require("../controllers/AppController");
-const StudentsController = require("../controllers/StudentsController");
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const StudentsController = require('../controllers/StudentsController');
 
 const app = express();
 
-app.get("/", (request, response) =>
-  AppController.getHomepage(request, response)
-);
+app.get('/', (request, response) => AppController.getHomepage(request, response));
 
-app.get("/students", (request, response) =>
-  StudentsController.getAllStudents(request, response)
-);
+app.get('/students', (request, response) => StudentsController.getAllStudents(request, response));
 
-app.get("/students/:major", (request, response) =>
-  StudentsController.getAllStudentsByMajor(request, response)
-);
+app.get('/students/:major', (request, response) => StudentsController.getAllStudentsByMajor(request, response));
 
 module.exports = app;
