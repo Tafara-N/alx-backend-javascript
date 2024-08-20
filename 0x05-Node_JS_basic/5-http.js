83b08e8538963ce15f0ff6120f617dce3318027b
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-20 16:39:33 +0200

    A more complex HTTP server using Node's HTTP module.

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -10,8 +10,8 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
-      if (err) return reject(Error('Cannot load the database'));
+    fs.readFile(path, { encoding: 'utf-8' }, (error, data) => {
+      if (error) return reject(Error('Cannot load the database'));
 
       const lines = data.split('\n').slice(1, -1);
 
@@ -73,8 +73,8 @@ const app = http.createServer((req, res) => {
         res.write(data.listStudents.join('\n'));
         res.end();
       })
-      .catch((error) => {
-        res.end(error.message);
+      .catch((erroror) => {
+        res.end(erroror.message);
       });
   }
 });
