c09797cf657a417ae92d107f83a9eb29ec1cb63c
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-20 14:22:51 +0200

    A small HTTP server using Node's HTTP module.

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -10,7 +10,7 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.write('Hello Holberton School!');
