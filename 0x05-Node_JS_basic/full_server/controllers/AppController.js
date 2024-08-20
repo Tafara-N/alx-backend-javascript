d2eeeab753ee5cfcc60d858070f6f9d55a8f0464
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-20 21:31:03 +0200

    Method returns a 200 status code and 'Hello Holberton School!'

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,9 @@
+/**
+ * AppController - This class contains the methods for the application
+ */
+
+module.exports = class AppController {
+  static getHomepage (request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+};
