3bd8c0df70586d1644c8a662c61abd85e288c33c
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-08-23 17:46:17 +0200

    Basic Integration testing.

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,14 @@
+//  Basic Integration testing
+
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.end('Welcome to the payment system');
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
